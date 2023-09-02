7d59ec3a151fdd42c204c2d1651ca86972530f25
Wendy <wperelstein@gmail.com>
2023-09-02 17:19:21 -0600

    CSS File Architecture continued

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 const postCSSPlugins = [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer'),
@@ -13,8 +14,16 @@ module.exports = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
+    devServer: {
+        watchFiles: ["app/**/*.html"],
+        static: {
+            directory: path.join(__dirname, "app"),
+            watch: false
+        },
+        hot: true,
+        port: 3000
+    },
     mode: 'development',
-    watch: true,
     module: {
         rules: [
             {
