d7f342574cc4a8b33eec832bb3fb73bb8d3d9b73
Wendy <wperelstein@gmail.com>
2023-09-03 10:53:59 -0600

    Image Resolution reference to top of App.js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,6 @@
+// for image info. see "C:\Users\wpere\OneDrive\Course Work\sites\responsive-images\Mastering the Modern Workflow example"
+// in terminal **npm run dev**
+
 import '../styles/styles.css'
 
 if (module.hot) {
