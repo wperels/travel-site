b72206a2a79b3a198933bffffdd153514b50a033
Wendy <wperelstein@gmail.com>
2024-01-15 13:31:29 -0700

    deleted Person.js used for learning.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,7 @@
 // in terminal **npm run dev**
 
 import '../styles/styles.css'
- // use this when importing from Person.js
-import Person from './modules/Person'
+
 // import MobileMenu from './modules/MobileMenu'
 //import RevealOnScroll from './modules/RevealOnScroll'
 
@@ -17,17 +16,3 @@ import Person from './modules/Person'
     module.hot.accept()
 } */
 
-//Lession below this line*/
-
-class Adult extends Person {
-    payTaxes() {
-        console.log("Oh, hello! " + this.name + " now owes zero taxes. ");
-    }
-}
-
-var john = new Person("John Doe", "purple");
-john.greet();
-
-var jane = new Adult("Jane Smith", "green");
-jane.greet();
-jane.payTaxes();
